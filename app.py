13eb8778d52c6d95ccf72838f0c455548cabd23a
mgadmin <mgstudio884@gmail.com>
2020-02-25 12:57:42 +0330

    next step of flask project

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_sms")
+def get_sms(): 
+    '''this function get and show sms'''
+    pass
+
+@app.route("/send_sms")
+def send_sms(): 
+    '''this function send sms'''
+    pass
 
 if __name__ == "__main__":
-    app.run()
+    app.run("0.0.0.0",5000,debug=True)
