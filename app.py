da48af42edb9aa252f4f2588ec084399364febb4
mgadmin <mgstudio884@gmail.com>
2020-02-25 13:30:50 +0330

    its sms api for it then

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def get_sms():
 @app.route("/send_sms")
 def send_sms(): 
     '''this function send sms'''
-    pass
+    sendsms(phone,mess)
 
 def sendsms(phone,mess):
-    api_key ='44345A386669384A683450384D2B434A656F5176326937594E6866734136754A2B4B565750415A6A6D66633D'
+    api_key='44345A386669384A683450384D2B434A656F5176326937594E6866734136754A2B4B565750415A6A6D66633D'
     url = 'https://api.kavenegar.com/v1/%s/sms/send.json' % (api_key)
     data = {'receptor':phone, 'message':mess}
     respon = requests.post(url,data=data)
@@ -28,4 +28,3 @@ def sendsms(phone,mess):
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5000,debug=True)
-    sendsms(phone,mess)
