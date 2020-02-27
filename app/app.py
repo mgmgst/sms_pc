508f7ba16e63c6e03a8a404268d96cd2048c01ff
mgadmin <mgstudio884@gmail.com>
2020-02-27 23:25:32 +0330

    adding login page

--- app/app.py
+++ app/app.py
@@ -19,12 +19,34 @@ def send_sms():
         return redirect("message_Send")        
 
     else:
-        return render_template('index.html')	
+        return render_template('index.html')
+
+@app.route("/",methods=["GET", "POST"])
+def login():
+    '''this function return login page'''
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if check(username,password):
+            return redirect("send_sms")
+        else:
+            return redirect("/")    
+    else:
+        return render_template("login.html")
 
 def sendsms(phone,mess):
     url = config.url
     data = {'receptor':phone, 'message':mess}
     respon = requests.post(url,data=data)
     return respon
+
+def check(username,password):
+    res = False
+    usernamein = config.username
+    passwordin = config.password
+    if username == usernamein and password == passwordin:
+        res = True
+    return res    
+
 if __name__ == "__main__":
     app.run("0.0.0.0",5000,debug=True)
