fd20c3c3badec9954e44acf5376d71910e59eb00
mgadmin <mgstudio884@gmail.com>
2020-02-27 13:49:09 +0330

    adding web pages

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import requests
-from flask import Flask , jsonify
+import config
+from flask import Flask , jsonify , redirect , request
 
 app = Flask(__name__)
 
-phone = "09155520952"
-mess = input("[+] harchi mikhaibego ta brat be shomarat ersal konam : ")
-
+@app.route("/message_send")
+def message_send(): 
+    '''this function get and show sms'''
+    return " * message_send" , 200		
+		
 @app.route("/get_sms")
 def get_sms(): 
     '''this function get and show sms'''
-    print("[+] we are getting sms . . . :)");
+    print("[+] we are getting sms . . . :)")
     data = {"message":"procecd"}
     return jsonify(data) , 200
 
-@app.route("/send_sms")
+@app.route("/send_sms",methods=["GET", "POST"])
 def send_sms(): 
     '''this function send sms'''
-    sendsms(phone,mess)
-    return f"sms send to {phone} \n message was : {mess}"
+    if request.method == 'POST':
+        message = request.form["message"]
+	    phone = request.form["phone"]
+	    sendsms(phone,message)
+	    return redirect("message_send")
+    else:
+        return render_template('index.html')	
 
 def sendsms(phone,mess):
-    api_key='44345A386669384A683450384D2B434A656F5176326937594E6866734136754A2B4B565750415A6A6D66633D'
-    url = 'https://api.kavenegar.com/v1/%s/sms/send.json' % (api_key)
+    url = config.url
     data = {'receptor':phone, 'message':mess}
     respon = requests.post(url,data=data)
-    print(respon)
-    print(respon.json())
-
 if __name__ == "__main__":
     app.run("0.0.0.0",5000,debug=True)
